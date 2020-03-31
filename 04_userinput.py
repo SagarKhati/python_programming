5a4fd2b332fbcbd86bb2f83c7cd466d84612172c
SagarKhati <sonu06sagar@gmail.com>
2020-03-31 23:38:45 +0530

    user input char

--- 04_userinput.py
+++ 04_userinput.py
@@ -14,3 +14,12 @@ x = int(input("Enter 1st number : "))
 y = int(input("Enter 2nd number : "))
 z = x + y
 print(z)
+
+ch = input('Enter a char : ')
+print(ch)
+
+ch = input('Enter a char : ')
+print(ch[0])
+
+ch = input('Enter a char : ')[0]
+print(ch)
