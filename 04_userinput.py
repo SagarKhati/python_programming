b6a4f357c7c8f485888fc4483bf44617f16a739e
SagarKhati <sonu06sagar@gmail.com>
2020-03-31 23:35:34 +0530

    user input int

---
+++ 04_userinput.py
@@ -0,0 +1,16 @@
+x = input("Enter 1st number : ")
+y = input("Enter 2nd number : ")
+z = x + y
+print(z)
+
+x = input("Enter 1st number : ")
+a = int(x)
+y = input("Enter 2nd number : ")
+b  = int(y)
+z = a + b
+print(z)
+
+x = int(input("Enter 1st number : "))
+y = int(input("Enter 2nd number : "))
+z = x + y
+print(z)
