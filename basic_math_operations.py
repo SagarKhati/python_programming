1c7003f80cb6f93924651baeb16c77759e573601
SagarKhati <sonu06sagar@gmail.com>
2020-04-01 13:35:39 +0530

    basic math opearations

---
+++ basic_math_operations.py
@@ -0,0 +1,16 @@
+''' Basic Maths Operations '''
+
+a = 7.0
+b = 11.0
+c = 7
+d = 11
+
+print( a+b)
+print( a-b)
+print( a*b)
+print( a/b)
+print( c/d)
+print( c**2)
+print( a%b)
+print( b%a)
+print( d%c)
