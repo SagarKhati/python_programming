1c6eaa8d5d30f98a7bbab11e281825abe40872a3
SagarKhati <sonu06sagar@gmail.com>
2020-04-01 13:31:37 +0530

    float data type

---
+++ float_datatype.py
@@ -0,0 +1,13 @@
+print(type(5.0))
+print(type(5))
+print(float(5))
+print(float('44'))
+#print(float(1j)) #type error
+print(7.6 + 8.7)
+print(round(7.6+8.7, 1))
+print(1.1 + 1.1 + 1.1 == 3.3)
+print(1.1 + 1.1 + 1.1)
+print(round(7.6 + 8.7, 2))
+
+v = 10.6452348
+print(round(v, 2))
