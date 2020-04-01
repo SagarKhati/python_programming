6e263c7216a9f9be15da965641bb0b71e42eb3e0
SagarKhati <sonu06sagar@gmail.com>
2020-04-01 13:21:22 +0530

    int data type

---
+++ int_datatype.py
@@ -0,0 +1,18 @@
+print(type(4))
+print(type(-4))
+print(type(4.0))
+print(type(1j))
+print(int('3'))
+print(int(5.4))
+print(int(5.9))
+print(int(-6.7))
+#print(int(1j)) # type error
+
+x = 6
+y = 12
+m = 0b1011
+n = 0xad4
+print(type(x))
+print(type(y))
+print(type(m))
+print(type(n))
