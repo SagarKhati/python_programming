fcee5e84979e9d015b8faa502ab1098de23a489d
SagarKhati <sonu06sagar@gmail.com>
2020-04-01 13:44:57 +0530

    bool data type

---
+++ bool_datatype.py
@@ -0,0 +1,37 @@
+print(True)
+#print(true) #NameError
+print(False)
+#print(false) #NameError
+
+a = 3
+b = 5
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+
+print(type(True))
+print(type(False))
+
+print(bool(28))
+print(bool(-2.71828))
+print(bool(0))
+
+print(bool('Turing'))
+print(bool(" "))
+print(bool(""))
+
+#trivial --> False
+#non-trivial --> True
+
+print(str(True))
+print(str(False))
+
+print(int(True))
+print(int(False))
+
+print(5 + True)
+print(10 * False)
+
+# 1 <--> True
+# 0 <--> False
