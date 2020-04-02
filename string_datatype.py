7977317f0ccdcc6520b008d687e43d8269850712
SagarKhati <sonu06sagar@gmail.com>
2020-04-02 16:07:16 +0530

    string data type

---
+++ string_datatype.py
@@ -0,0 +1,25 @@
+welcome = "Hello World"
+print(id(welcome))
+print(type(welcome))
+print(welcome)
+
+print(welcome)
+print(welcome[0])
+print(welcome[-11])
+
+print(welcome)
+print(welcome[5])
+print(welcome[-6])
+
+print(welcome)
+print(welcome[8])
+print(welcome[-3])
+
+s1  = "Py" + 'thon'
+print(s1)
+s2 = 'Python is amazing'
+print(s2)
+s3 = 'Python\nis\namazing'
+print(s3)
+s4 = 'Python\tis\tamazing'
+print(s4)
