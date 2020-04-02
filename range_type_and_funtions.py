d445b567b625303cccfa6c196a6a24ff629255ec
SagarKhati <sonu06sagar@gmail.com>
2020-04-02 19:24:59 +0530

    rnage type and functions

---
+++ range_type_and_funtions.py
@@ -0,0 +1,36 @@
+a = list(range(6))
+print(a)
+
+b = tuple(range(6))
+print(b)
+
+c = tuple(range(100))
+print(c)
+
+d = list(range(1,10))
+print(d)
+
+
+e = list(range(0, 11, 2))
+print(e)
+
+f = list(range(-2, -11, 2))
+print(f)
+
+g = list(range(-2, -11, -2))
+print(g)
+
+h = list(range(-2, -11, -2))
+print(h)
+
+k1 = list(range(0, 5))
+print(k1)
+
+k2 = list(range(10, 15))
+print(k2)
+
+k3 = list(range(10, 21, 2))
+print(k3)
+
+k4 = list(range(100, 0, -25))
+print(k4)
