763f5ab86c9085abb2d9424ffaa62fd04655a19c
SagarKhati <sonu06sagar@gmail.com>
2020-04-02 20:59:02 +0530

    if else conditions

---
+++ if_else_conditions.py
@@ -0,0 +1,9 @@
+score = 68.3
+if  score >= 90:
+    print('Distinct')
+elif score >= 60 and score < 90:
+    print('Above average')
+elif score >= 40 and score < 60:
+    print('Average')
+else:
+    print('Fail')
