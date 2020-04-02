e6d82148b736b99b29b814feb9b0f5dd51fafda6
SagarKhati <sonu06sagar@gmail.com>
2020-04-02 19:00:51 +0530

    tuple type

---
+++ tuple_type.py
@@ -0,0 +1,14 @@
+a = (123, 12.23, "demo")
+print(a[1])
+
+#a[1] = 123 #TypeError: 'tuple' object does not support item assignment
+
+print(a[0:2])
+
+tup1=tuple()
+print(tup1)
+
+tup2=tuple('Welcome')
+print(tup2)
+print(tup2.count('e'))
+print(len(tup2))
